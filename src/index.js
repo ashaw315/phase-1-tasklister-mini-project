23ec3066b9f881365d7fc253c3f8354216b930be
Adam <ashaw315@gmail.com>
2021-11-28 12:59:38 -0500

    updated java

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target.new_task_description.value)
+    form.reset()
+
+  })
+
+})
+
+function handleToDo(todo) {
+  let p = document.createElement('p')
+  p.textContent = `${todo}  `
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
+
+
+
+
+
+
